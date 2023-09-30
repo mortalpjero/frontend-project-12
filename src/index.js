89df6e53445aba42ccdcf26380a44da9b3cc7d4f
mortal pjero <mortalpjero@gmail.com>
2023-09-30 15:31:09 +0100

    feat: test deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='baya'>Бая, ты мой пупсик</div>
   </React.StrictMode>
 );
 
