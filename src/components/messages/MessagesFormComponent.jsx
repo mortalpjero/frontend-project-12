08768ee4c4d66ab0a8ea9681d845ef5ad57d3b42
mortal pjero <mortalpjero@gmail.com>
2023-12-04 18:15:18 +0000

    fix: unable to send message if it is empty

--- src/components/messages/MessagesFormComponent.jsx
+++ src/components/messages/MessagesFormComponent.jsx
@@ -17,16 +17,18 @@ const MessagesFormComponent = () => {
       channelId: currChannel.id,
       username,
     };
-    emitNewMessage(newMessage, (confirmation) => {
-      const { status } = confirmation;
-      if (status === 'ok') {
-        setInputStatus('valid');
-      } else {
-        setInputStatus('invalid');
-        console.error('Message did not send, try again later');
-      }
-      setMessage('');
-    });
+    if (message.length > 0) {
+      emitNewMessage(newMessage, (confirmation) => {
+        const { status } = confirmation;
+        if (status === 'ok') {
+          setInputStatus('valid');
+        } else {
+          setInputStatus('invalid');
+          console.error('Message did not send, try again later');
+        }
+        setMessage('');
+      });
+    }
   };
 
   const onTyping = (value) => {
@@ -43,7 +45,7 @@ const MessagesFormComponent = () => {
     <Form novalidate="" className="py-1 border rounded-2" onSubmit={(e) => handleSendMessage(e)}>
       <Form.Group className="input-group has-validation">
         <Form.Control name="body" aria-label="Новое сообщение" placeholder="Введите сообщение..." className="border-0 p-0 ps-2 form-control" value={message} onChange={(e) => onTyping(e.target.value)} />
-        <Button variant="outline-secondary" type="submit" disabled={inputStatus === 'invalid' || !navigator.onLine} className="btn btn-group-vertical border-0">
+        <Button variant="outline-secondary" type="submit" disabled={inputStatus === 'invalid' || !navigator.onLine || message.length === 0} className="btn btn-group-vertical border-0">
           <svg xmlns="http://www.w3.or g/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
             <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
           </svg>
