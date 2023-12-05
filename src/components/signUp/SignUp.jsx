2adb774fb94f5699e2e10e8236e0513a54f3559a
mortal pjero <mortalpjero@gmail.com>
2023-12-05 16:07:10 +0000

    feat: added redirect if there is a token

--- src/components/signUp/SignUp.jsx
+++ src/components/signUp/SignUp.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
 import HeaderComponent from '../header/HeaderComponent';
 import SignUpForm from './SignUpForm';
 import imgPath from '../../img/hexletSignUpImage.jpg';
 
+const SignUpSchema = Yup.object().shape({
+  username: Yup.string()
+    .required('Обязательное поле')
+    .min(3, 'Имя должно быть от 3 до 20 символов')
+    .max(20, 'Имя должно быть от 3 до 20 символов'),
+  password: Yup.string()
+    .required('Обязательное поле')
+    .min(6, 'Не менее 6 символов'),
+  confirmPassword: Yup.string()
+    .required('Обязательное поле')
+    .oneOf([Yup.ref('password'), null], 'Пароли должны совпадать'),
+});
+
 const SignUp = () => {
-  const SignUpSchema = Yup.object().shape({
-    username: Yup.string()
-      .required('Обязательное поле')
-      .min(3, 'Имя должно быть от 3 до 20 символов')
-      .max(20, 'Имя должно быть от 3 до 20 символов'),
-    password: Yup.string()
-      .required('Обязательное поле')
-      .min(6, 'Не менее 6 символов'),
-    confirmPassword: Yup.string()
-      .required('Обязательное поле')
-      .oneOf([Yup.ref('password'), null], 'Пароли должны совпадать'),
+  const navigate = useNavigate();
+  const authToken = localStorage.getItem('Authorization');
+  useEffect(() => {
+    if (authToken) {
+      navigate('/');
+    }
   });
 
   return (
